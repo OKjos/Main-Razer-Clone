9ea4dbfdb71d05a06317ef6a140bdea8fa95eb6a
Joshua <113487918+OKjos@users.noreply.github.com>
2023-11-13 21:29:47 -0500

    Made awarness learn more button link to its html

--- Main.js
+++ Main.js
@@ -5,9 +5,14 @@
 // });
 
 
-var awarenesslearn = document.getElementById("awarenesslearn");
-if (awarenesslearn) {
-    button.addEventListener('click', function() {
-        window.location.href = "Awareness.html";
-    });
-} 
+document.addEventListener('DOMContentLoaded', function() {
+    var awarenesslearn = document.getElementById("awarenesslearn");
+
+    if (awarenesslearn) {
+        awarenesslearn.addEventListener('click', function() {
+            window.location.href = "Pages/AWARENESS/Awareness.html";
+        });
+    } else {
+        console.error("Button with ID 'awarenesslearn' not found.");
+    }
+});
