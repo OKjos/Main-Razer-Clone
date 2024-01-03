5d4e6ea246ce7e248340de6cd577c64f7b702ed4
Joshua <113487918+OKjos@users.noreply.github.com>
2024-01-03 13:41:32 -0500

    newage button alignments finished, submit button gives error

--- Pages/NEWAGETECH/Newage.js
+++ Pages/NEWAGETECH/Newage.js
@@ -2,7 +2,33 @@ const buttons = document.querySelectorAll('.button');
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        buttons.forEach(btn => btn.classList.remove('active')); // Remove 'active' class from all buttons
-        this.classList.add('active'); // Add 'active' class to the clicked button
+        buttons.forEach(btn => btn.classList.remove('active')); 
+        this.classList.add('active'); 
+    });
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.final-submit-button');
+    const errorContainer = document.querySelector('.error-container');
+
+    button.addEventListener('click', function() {
+        const errorMessage = document.createElement('div');
+        errorMessage.classList.add('error-message');
+        errorMessage.textContent = 'Error: Failed to send';
+        
+        const existingErrorMessage = errorContainer.querySelector('.error-message');
+        if (existingErrorMessage) {
+            errorContainer.removeChild(existingErrorMessage);
+        }
+        
+        errorContainer.appendChild(errorMessage);
+
+        setTimeout(function() {
+            errorContainer.removeChild(errorMessage);
+        }, 1000); 
     });
 });
